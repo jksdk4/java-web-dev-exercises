a22c3beffd0c66ccdbb913dca8098428df3942cf
Jasmine <jasspears@gmail.com>
2020-01-06 18:34:59 -0600

    Completed basic studio

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,17 @@
+package org.launchcode.java.studios.areaofacircle;
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args){
+        double radius;
+        Scanner input;
+
+        input = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        radius = input.nextDouble();
+        input.close();
+
+        double area = Circle.getArea(radius);
+        System.out.println("The area of a circle with radius " + radius + " is " + area);
+    }
+}
